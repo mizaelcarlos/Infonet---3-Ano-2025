faa29b4545d126ba7baf03e80d33f52d6eee1de9
Mizael Carlos <mizaelcarlos44@gmail.com>
2025-05-08 07:19:23 -0300

    Lista de arrays

---
+++ Programação Client-Side/Exercicio/lista3.js
@@ -0,0 +1,94 @@
+//Questão 1
+// for (let n = 0; n <= 50; n++) {
+//   console.log(n);
+//   if (n % 17 == 0) {
+//     break;
+//   }
+// }
+
+//Questão 2
+// for (let n = 1; n <= 500; n = n * 3) {
+//   console.log(n);
+// }
+
+// var n = 1;
+// while (n <= 500) {
+//   console.log(n);
+//   n = n * 3;
+// }
+
+// var n = 1;
+// do {
+//   console.log(n);
+//   n = n * 3;
+// } while (n <= 500);
+
+// for (let n = 1; n <= 100; n++) {
+//   if (n % 2 != 0) {
+//     console.log(n);
+//   }
+// }
+
+// var n = 1;
+// while (n <= 100) {
+//   if (n % 2 != 0) {
+//     document.write("<p>" + n + "</p>");
+//   }
+//   n++;
+// }
+
+// var n = 1;
+// do {
+//   if (n % 2 != 0) {
+//     document.write("<p>" + n + "</p>");
+//   }
+//   n++;
+// } while (n <= 100);
+
+// var senha;
+// do {
+//   senha = prompt("Digite a senha");
+// } while (senha != "1234");
+
+// var opcao;
+// do {
+//   opcao = prompt("Informe a opção 1 - Continuar e 2 - Sair");
+// } while (opcao != "2");
+
+// for (let n = 100; n >= 0; n = n - 5) {
+//   console.log(n);
+// }
+
+// var tabuada = "";
+// var numero = 7;
+// for (let n = 1; n <= 10; n++) {
+//   tabuada = numero + "+" + n + "=" + (numero + n);
+//   console.log(tabuada);
+//   tabuada = numero + "x" + n + "=" + numero * n;
+//   console.log(tabuada);
+//   tabuada = numero + "/" + n + "=" + numero / n;
+//   console.log(tabuada);
+//   tabuada = numero + "-" + n + "=" + (numero - n);
+//   console.log(tabuada);
+// }
+
+// var soma = 0;
+// for (let n = 1; n <= 100; n++) {
+//   if (n % 4 == 0) {
+//     soma = soma + n;
+//   }
+// }
+// console.log(soma);
+
+// var fatorial = 1;
+// for (let n = 1; n <= 5; n++) {
+//   fatorial = fatorial * n;
+// }
+// console.log(fatorial);
+var soma = 0;
+for (let n = 1; n <= 100; n++) {
+  if (n % 11 == 0) {
+    soma++;
+  }
+}
+console.log(soma);
